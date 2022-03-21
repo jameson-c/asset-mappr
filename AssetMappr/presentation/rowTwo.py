30af2c2c644ff3945aa7c8370b53b481ebfe2b1e
annawangkkk <91227655+annawangkkk@users.noreply.github.com>
2022-03-21 00:26:29 -0400

    Update rowTwo.py

--- AssetMappr/presentation/rowTwo.py
+++ AssetMappr/presentation/rowTwo.py
@@ -3,14 +3,14 @@ import dash_bootstrap_components as dbc
 
 
 def rowTwoLeft():
-    return html.Div("Explore your community! Search the map for assets in your town.",
+    return html.H3("Explore your community! Search the map for assets in your town.",
                     style={'font-family': 'Gill Sans', "border": "2px black solid", 'border-color':
                            'olivedrab', 'background': '#edede8', 'color': 'Black', 'font_size': '26px',
                            'text-align': 'center'})
 
 
 def rowTwoRight():
-    return html.Div("CLICK HERE to add to the inverntory!",
+    return html.H3("CLICK HERE to add to the inverntory!",
                     style={'background': 'olivedrab', 'color': 'Black',
                            'font-family': 'Gill Sans', 'font_size': '20px',
                            'text-align': 'center'})
