f1a634a052e898aaaa9e3cadffcf35193599053e
Jameson Carter <jamesonarmstrongcarter@gmail.com>
2022-05-06 15:19:37 -0400

    Updating files to comply with elastic beanstalk
    Per the following post- https://austinlasseter.medium.com/deploying-a-dash-app-with-elastic-beanstalk-console-27a834ebe91d
    
    Change reference to server to application, change file name, add to requirements file.

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ from AssetMappr.application.suggest_missing_asset_cb import suggest_missing_asse
 # =============================================================================
 app = dash.Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.FLATLY])
-server = app.server
+application = app.server
 
 app.title = 'AssetMappr'
 
@@ -87,4 +87,4 @@ suggest_missing_asset_cb(app)
 
 # Run the app
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    application.run(debug=True)
