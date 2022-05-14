29798be213dadc465ff491005ec3e79ca88e04e1
annawangkkk <annawang1009@hotmail.com>
2022-05-13 22:54:53 -0400

    Change layout based on Martin's comments
    Based on Martin's comments, do some changes

--- AssetMappr/application/submitRating_cb.py
+++ AssetMappr/application/submitRating_cb.py
@@ -19,22 +19,20 @@ Output:
 """
 import dash
 from dash.dependencies import Input, Output, State
-from dash import dash_table
-from dash import dcc
-from dash import html
-import pandas as pd
-from flask import request
+import dash_bootstrap_components as dbc
 
 from AssetMappr.database.submitRating_db import submitRating_db
 
+
 def submitRating_cb(app):
     @app.callback(
-        Output(component_id='submit-rating-confirmation', component_property='children'),
+        Output(component_id='submit-rating-confirmation',
+               component_property='children'),
         [Input('submit-rating-button', 'n_clicks')],
         [State('graph', 'clickData')],
         [State('rating-score', 'value')],
         [State('rating-comments', 'value')]
-        )
+    )
     def submit_rating(n_clicks, clickData, rating_score, rating_comments):
         # This callback is only triggered when someone clicks the submit button
         if n_clicks == 0:
@@ -42,8 +40,13 @@ def submitRating_cb(app):
         else:
             # Get Asset ID from the click data
             asset_id = clickData['points'][0]['customdata'][3]
-            
+
             # Write the rating information to the staged ratings table in the DB
             submitRating_db(asset_id, rating_score, rating_comments)
-            
-            return 'Your review has been submitted - thanks for sharing!'
+            return dbc.Alert('Your review \" \n{} \" has been submitted - Thanks for sharing! '.format(rating_comments), dismissable=True, color='success')
+
+    @app.callback(Output('rating-comments', 'value'),
+                  Input('submit-rating-button', 'n_clicks')
+                  )
+    def clear_persistence(n_clicks):
+        return " " if n_clicks else dash.no_update
